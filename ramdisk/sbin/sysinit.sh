87f8d11d9f9edc33f7ac8bd653270c2ddceba7c1
Vagelis1608 <vagelis1608@gmail.com>
2015-05-02 23:47:14 +0300

    Set SELinux to Permissive

--- ramdisk/sbin/sysinit.sh
+++ ramdisk/sbin/sysinit.sh
@@ -2,3 +2,8 @@
 
 # -V- Kernel Mods
 # by Vagelis1608 @xda-developers
+
+# Set SELinux to Permissive 
+# This should take place before init.d and userinit.d
+# to make sure that it won't interfere.
+setenforce 0
