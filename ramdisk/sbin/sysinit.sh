cfd228495b544a2c5af404c0c71afc4e530adc34
Vagelis1608 <vagelis1608@gmail.com>
2015-05-02 23:49:35 +0300

    Added init.d support

--- ramdisk/sbin/sysinit.sh
+++ ramdisk/sbin/sysinit.sh
@@ -7,3 +7,17 @@
 # This should take place before init.d and userinit.d
 # to make sure that it won't interfere.
 setenforce 0
+
+# Fix init.d folder
+mount -o rw,remount /system
+if [ ! -e /system/etc/init.d ] ; then
+     mkdir /system/etc/init.d
+fi
+chown -R root.root /system/etc/init.d
+chmod -R 755 /system/etc/init.d
+mount -o ro,remount /system
+
+# Start init.d
+for FILE in /system/etc/init.d/*; do
+     sh $FILE >/dev/null
+done;
