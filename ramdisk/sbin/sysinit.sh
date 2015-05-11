b75085f1e6ff6871f4160c38ba94aa3fad3afd21
Vagelis1608 <vagelis1608@gmail.com>
2015-05-11 20:04:21 +0300

    Changed ownership of init.d contents

--- ramdisk/sbin/sysinit.sh
+++ ramdisk/sbin/sysinit.sh
@@ -13,7 +13,8 @@ mount -o rw,remount /system
 if [ ! -e /system/etc/init.d ] ; then
      mkdir /system/etc/init.d
 fi
-chown -R root.root /system/etc/init.d
+chown root.root /system/etc/init.d
+chown root.shell /system/etc/init.d/*
 chmod -R 755 /system/etc/init.d
 mount -o ro,remount /system
 
