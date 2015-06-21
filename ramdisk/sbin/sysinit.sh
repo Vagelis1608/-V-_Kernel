572f8cf3de1a8598d55933ed41b442160abd8fea
Vagelis1608 <vagelis1608@gmail.com>
2015-06-22 00:08:08 +0300

    #(I lost the actual commits, so I made this)# Logging of init.d and userinit.d. Moved mods from init.rc to init.extra.rc

--- ramdisk/sbin/sysinit.sh
+++ ramdisk/sbin/sysinit.sh
@@ -11,20 +11,35 @@ setenforce 0
 # Fix init.d folder
 mount -o rw,remount /system
 if [ ! -e /system/etc/init.d ] ; then
-     mkdir /system/etc/init.d
+    mkdir /system/etc/init.d
 fi
 chown -R root.root /system/etc/init.d
 chmod -R 755 /system/etc/init.d
 mount -o ro,remount /system
 
 # Start init.d
+if [ -e /data/local/init.log ]; then
+    rm -f /data/local/init.log
+fi
+touch -p /data/local/init.log
 for FILE in /system/etc/init.d/*; do
-     sh $FILE >/dev/null
+    echo "Script: $FILE" >> /data/local/init.log
+    sh $FILE >> /data/local/init.log
+    echo "# End of $FILE" >> /data/local/init.log
+    echo "" >> /data/local/init.log
 done;
 
 # Start userinit.d
 if [ -e /sdcard/userinit.d ]; then
-     for FILE in /sdcard/userinit.d/*; do
-          sh $FILE >/dev/null
-     done;
+    if [ -e /data/local/userinit.log ]; then
+        rm -f /data/local/userinit.log
+    fi
+    touch -p /data/local/userinit.log
+    for FILE in /sdcard/userinit.d/*; do
+        echo "Script: $FILE" >> /data/local/userinit.log
+        sh $FILE >> /data/local/userinit.log
+        echo "# End of $FILE" >> /data/local/userinit.log
+        echo "" >> /data/local/userinit.log
+    done;
 fi
+
